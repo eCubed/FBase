81a7de95bcd682a3720a14b0a958f5d2771cfb08
Unknown <mr.i.d.fernando@gmail.com>
2020-02-29 07:03:39 -0600

    Begin DotNetFramework EF impl

---
+++ MyStuff.DotNet.EntityFramework/MyStuffDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace MyStuff.DotNet.EntityFramework
+{
+    public class MyStuffDbContext : DbContext
+    {
+        public DbSet<MyThing> MyThings { get; set; }
+
+        public MyStuffDbContext() : base()
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

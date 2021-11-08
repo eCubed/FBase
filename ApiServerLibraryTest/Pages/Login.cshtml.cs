7e157490d1b7cc20b444164ee8ecf257d3257a0c
Izzy <mr.i.d.fernando@gmail.com>
2021-11-08 14:13:59 -0600

    Sets up basic razor pages

---
+++ ApiServerLibraryTest/Pages/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiServerLibraryTest.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ApiServerLibraryTest.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+        private SignInManager<TestUser> SignInManager { get; set; }
+        private UserManager<TestUser> UserManager { get; set; }
+
+        public LoginModel(SignInManager<TestUser> signInManager, UserManager<TestUser> userManager)
+        {
+            SignInManager = signInManager;
+            UserManager = userManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            TestUser user = await UserManager.FindByNameAsync(Username);
+
+            if (user == null)
+            {
+                ViewData["ErrorMessage"] = "User does not exist";
+                return Page();
+                
+            } 
+            else
+            {
+                if (!await UserManager.CheckPasswordAsync(user, Password))
+                {
+                    ViewData["ErrorMessage"] = "Incorrect Password";
+                    return Page();
+                }
+                else
+                {
+                    await SignInManager.SignInAsync(user, false);
+                    return Redirect("/");
+                }
+            }
+        }
+    }
+}
---
+++ ApiServerLibraryTest/Pages/oauth/Authorize.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiServerLibraryTest.Data;
+using FBase.ApiServer.OAuth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ApiServerLibraryTest.Pages.oauth
+{
+    public class AuthorizeModel : PageModel
+    {
+        private CredentialSetManager<CredentialSet> CredentialSetManager { get; set; }
+        private AppManager<App, int> AppManager { get; set; }
+        
+        public bool ClientIdFound { get; set; }
+        public string AppName { get; set; }
+        public List<string> Scopes { get; set; }
+
+        public AuthorizeModel(ApiServerLibraryTestDbContext context)
+        {
+            CredentialSetManager = new CredentialSetManager<CredentialSet>(new CredentialSetStore(context));
+            AppManager = new AppManager<App, int>(new AppStore(context));
+        }
+
+
+        public async Task OnGet([FromQuery(Name = "client_id")] string clientId = "")
+        {
+            var credentialSet = await CredentialSetManager.FindByClientIdAsync(clientId);
+
+            if (credentialSet == null)
+            {
+                ClientIdFound = false;
+            } else
+            {
+                ClientIdFound = true;
+                var app = await AppManager.FindByIdAsync(credentialSet.AppId);
+                // app can't be null!
+                AppName = app.Name;
+                Scopes = AppManager.GetScopes(app.Id);
+
+            }
+
+            var dummy = 3;
+        }
+    }
+}
